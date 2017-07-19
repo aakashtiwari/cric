1cfe6da21ea9a795c8b9a1a125df0abb673d003e
aakash <akashtiwari82@gmail.com>
2017-07-19 17:49:29 +0530

    create cric app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cricApp
+ * @description
+ * # cricApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cricApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cricApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cricApp
+ */
+angular.module('cricApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
